7b71835f3d1488cebe0da4eac388124d59cfe49c
Steven Lai <stevenlai_dd@yahoo.com>
2013-04-19 12:04:59 +0800

    Some configurations for opening chm files

--- user-profile/bin/docbook.sh
+++ user-profile/bin/docbook.sh
@@ -1,3 +1,4 @@
-docbook_html(){
-	xsltproc --xinclude --output html/${1}.html /usr/share/sgml/docbook/xsl-stylesheets/xhtml5/docbook.xsl ${1}.xml
+docbook_html()
+{
+	xsltproc --xinclude --output "html/${1}.html" /usr/share/sgml/docbook/xsl-stylesheets/xhtml5/docbook.xsl "${1}.xml"
 }
