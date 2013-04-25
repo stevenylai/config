57dce921770e3cebbf4d732ac018b7ecb5110660
Steven Lai <stevenlai_dd@yahoo.com>
2013-04-25 20:21:03 +0800

    Modified docbook scripts

--- user-profile/bin/docbook.sh
+++ user-profile/bin/docbook.sh
@@ -2,3 +2,9 @@ docbook_html()
 {
 	xsltproc --xinclude --output "html/${1}.html" /usr/share/sgml/docbook/xsl-stylesheets/xhtml5/docbook.xsl "${1}.xml"
 }
+docbook_pdf()
+{
+	xsltproc --xinclude /usr/share/sgml/docbook/xsl-ns-stylesheets/fo/docbook.xsl "${1}.xml" > "${1}.fo"
+	fop -fo "${1}.fo" -pdf "${1}.pdf"
+	rm "${1}.fo"
+}
