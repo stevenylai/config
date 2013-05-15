f260c1641f0c8568d22194ed21f07ed271f4111f
Steven Lai <stevenlai_dd@yahoo.com>
2013-05-15 16:21:03 +0800

    Updated the startup script to reflect the latest versions of the tools

--- home/user/bin/liricco.sh
+++ home/user/bin/liricco.sh
@@ -1,12 +1,12 @@
-if [ -d /opt/buildroot-gcc342/bin ]; then
-	export PATH=${PATH}:/opt/buildroot/usr/bin
+if [ -d /opt/buildroot/bin ]; then
+	export PATH=${PATH}:/opt/buildroot/bin
 fi
 if [ -d /opt/sdcc/bin ]; then
 	export PATH=${PATH}:/opt/sdcc/bin
 fi
 li_env() {
 	if [ -n "`uname|grep -i cygwin`" ]; then
-		export PATH=/usr/local/bin:/usr/bin:${HOME}/bin:/opt/liricco/bin
+		export PATH=/usr/local/bin:/usr/bin:${HOME}/bin
 	fi
 }
 alias li_fs='ssh liriccoserver@fs-t110ii'
