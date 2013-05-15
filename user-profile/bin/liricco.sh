d7e62fea909d552249a021c8c7b3c152d9449615
Steven Lai <stevenlai_dd@yahoo.com>
2013-05-15 10:05:44 +0800

    Created the office branch

--- user-profile/bin/liricco.sh
+++ user-profile/bin/liricco.sh
@@ -9,5 +9,8 @@ li_env() {
 		export PATH=/usr/local/bin:/usr/bin:${HOME}/bin:/opt/liricco/bin
 	fi
 }
+chub_devel() {
+	mount --bind /dev /mnt/liricco/smb_chub-devel/dev && mount --bind /sys /mnt/liricco/smb_chub-devel/sys && mount --bind /proc /mnt/liricco/smb_chub-devel/proc && cp -L /etc/resolv.conf /mnt/liricco/smb_chub-devel/etc/ && chroot /mnt/liricco/smb_chub-devel /bin/bash
+}
 alias li_fs='ssh liriccoserver@fs-t110ii'
 alias li_web='ssh liriccoweb@liricco.com'
