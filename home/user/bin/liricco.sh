e892f74e15a125ac7f01df879493424248dc9ce0
Steven Lai <stevenlai_dd@yahoo.com>
2013-05-17 16:41:42 +0800

    Added some keywords for necessary tools for building chub.

--- home/user/bin/liricco.sh
+++ home/user/bin/liricco.sh
@@ -9,5 +9,10 @@ li_env() {
 		export PATH=/usr/local/bin:/usr/bin:${HOME}/bin
 	fi
 }
+export CHUB_TEMP_IMG=/tftproot/Image
+export CHUB_FINAL_IMG=/tftproot/Kernel.bin
+chub_image() {
+	mipsel-softfloat-linux-uclibc-objcopy -O binary -R .note -R .comment -S "${1}" "${CHUB_TEMP_IMG}" && rm -f "${CHUB_TEMP_IMG}.lzma" && lzma -9 -f -S .lzma "${CHUB_TEMP_IMG}" && mkimage -A mips -O linux -T kernel -C lzma -a 80000000 -e 0x8027c000 -n "Linux Kernel Image" -d "${CHUB_TEMP_IMG}.lzma" "${CHUB_FINAL_IMG}"
+}
 alias li_fs='ssh liriccoserver@fs-t110ii'
 alias li_web='ssh liriccoweb@liricco.com'
