215c18caea6952289c6f45f11f01ed369bbea388
Steven Lai <steven.lai@liricco.com>
2013-05-25 13:28:34 +0800

    Some new cross compiler stuff

--- home/user/bin/liricco.sh
+++ home/user/bin/liricco.sh
@@ -12,7 +12,11 @@ li_env() {
 export CHUB_TEMP_IMG=/tftproot/Image
 export CHUB_FINAL_IMG=/tftproot/Kernel.bin
 chub_image() {
-	mipsel-softfloat-linux-uclibc-objcopy -O binary -R .note -R .comment -S "${1}" "${CHUB_TEMP_IMG}" && rm -f "${CHUB_TEMP_IMG}.lzma" && lzma -9 -f -S .lzma "${CHUB_TEMP_IMG}" && mkimage -A mips -O linux -T kernel -C lzma -a 80000000 -e 0x8027c000 -n "Linux Kernel Image" -d "${CHUB_TEMP_IMG}.lzma" "${CHUB_FINAL_IMG}"
+	if [ $# -lt 1 ]; then
+		mipsel-unknown-linux-uclibc-objcopy -O binary -R .note -R .comment -S vmlinux "${CHUB_TEMP_IMG}" && /mnt/liricco/sftp_web/bin/lzma -9 -f -S .lzma "${CHUB_TEMP_IMG}" && mkimage -A mips -O linux -T kernel -C lzma -a 80000000 -e 0x8027c000 -n "Linux Kernel Image" -d "${CHUB_TEMP_IMG}.lzma" "${CHUB_FINAL_IMG}"
+	else
+		dtb-patch vmlinux "${1}" && mipsel-unknown-linux-uclibc-objcopy -O binary -R .note -R .comment -S vmlinux "${CHUB_TEMP_IMG}" && /mnt/liricco/sftp_web/bin/lzma -9 -f -S .lzma "${CHUB_TEMP_IMG}" && mkimage -A mips -O linux -T kernel -C lzma -a 80000000 -e 0x8027c000 -n "Linux Kernel Image" -d "${CHUB_TEMP_IMG}.lzma" "${CHUB_FINAL_IMG}"
+	fi
 }
 alias li_fs='ssh liriccoserver@fs-t110ii'
 alias li_web='ssh liriccoweb@liricco.com'
