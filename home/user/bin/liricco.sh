5ad90845d8f77f8f6f2dacb2ec4e66475ac66d90
Steven Lai <steven.lai@liricco.com>
2013-06-01 01:46:31 +0800

    Changed some paths

--- home/user/bin/liricco.sh
+++ home/user/bin/liricco.sh
@@ -1,5 +1,5 @@
-if [ -d /opt/buildroot/bin ]; then
-	export PATH=${PATH}:/opt/buildroot/bin
+if [ -d /opt/buildroot-gcc342/bin ]; then
+	export PATH=${PATH}:/opt/buildroot-gcc342/bin
 fi
 if [ -d /opt/sdcc/bin ]; then
 	export PATH=${PATH}:/opt/sdcc/bin
@@ -9,8 +9,8 @@ li_env() {
 		export PATH=/usr/local/bin:/usr/bin:${HOME}/bin
 	fi
 }
-export CHUB_TEMP_IMG=/tftproot/Image
-export CHUB_FINAL_IMG=/tftproot/Kernel.bin
+export CHUB_TEMP_IMG=/tftpboot/Image
+export CHUB_FINAL_IMG=/tftpboot/Kernel.bin
 chub_image() {
 	if [ $# -lt 1 ]; then
 		mipsel-unknown-linux-uclibc-objcopy -O binary -R .note -R .comment -S vmlinux "${CHUB_TEMP_IMG}" && /mnt/liricco/sftp_web/bin/lzma -9 -f -S .lzma "${CHUB_TEMP_IMG}" && mkimage -A mips -O linux -T kernel -C lzma -a 80000000 -e 0x8027c000 -n "Linux Kernel Image" -d "${CHUB_TEMP_IMG}.lzma" "${CHUB_FINAL_IMG}"
