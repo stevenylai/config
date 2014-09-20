aa3eba5d5de51103acf258fbd6526d55de28b291
Steven Lai <stevenylai@gmail.com>
2014-09-20 11:39:25 +0800

    Add a path checking before setting ITS related variables

--- home/user/bin/its.sh
+++ home/user/bin/its.sh
@@ -5,12 +5,14 @@ else
     CYGPATH="echo"
     SEP=":"
 fi
-export CLASSPATH="${CLASSPATH}${SEP}`${CYGPATH} ${HOME}/work/src/its_demo/embedded/tools/java`"
-JARS="`find ${HOME}/work/src/its_demo/embedded/tools/java/third_party -name '*.jar'|xargs ${CYGPATH}|sed ':a;N;$!ba;s/\n/|/g'`"
-if [ -n "`uname|grep -i cygwin`" ]; then
-    JARS="`echo ${JARS}|sed 's/|/;/g'`"
-else
-    JARS="`echo ${JARS}|sed 's/|/:/g'`"
+if [ -d ${HOME}/work/src/its_demo ]; then
+    export CLASSPATH="${CLASSPATH}${SEP}`${CYGPATH} ${HOME}/work/src/its_demo/embedded/tools/java`"
+    JARS="`find ${HOME}/work/src/its_demo/embedded/tools/java/third_party -name '*.jar'|xargs ${CYGPATH}|sed ':a;N;$!ba;s/\n/|/g'`"
+    if [ -n "`uname|grep -i cygwin`" ]; then
+	JARS="`echo ${JARS}|sed 's/|/;/g'`"
+    else
+	JARS="`echo ${JARS}|sed 's/|/:/g'`"
+    fi
+    export CLASSPATH="${CLASSPATH}${SEP}${JARS}"
 fi
-export CLASSPATH="${CLASSPATH}${SEP}${JARS}"
 
